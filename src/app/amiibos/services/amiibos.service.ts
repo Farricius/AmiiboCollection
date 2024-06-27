d8f8a465fb7f911b2fe16f0362a9b4fb5258b175
Farricius <jose.hernandez.chacon.al@iespoligonosur.org>
2024-06-27 05:08:35 +0200

    Deploy error with slash fixed

--- src/app/amiibos/services/amiibos.service.ts
+++ src/app/amiibos/services/amiibos.service.ts
@@ -13,28 +13,28 @@ export class AmiibosService {
   constructor(private http: HttpClient) {}
 
   searchByName(term: string): Observable<Amiibo[]> {
-    const url = `${this.baseUrl}/amiibo/?name=${term}&type=figure`;
+    const url = `${this.baseUrl}amiibo/?name=${term}&type=figure`;
     return this.http
       .get<{ amiibo: Amiibo[] }>(url)
       .pipe(map((response) => response.amiibo));
   }
 
   searchByAmiiboSeries(term: string): Observable<Amiibo[]> {
-    const url = `${this.baseUrl}/amiibo/?amiiboSeries=${term}&type=figure`;
+    const url = `${this.baseUrl}amiibo/?amiiboSeries=${term}&type=figure`;
     return this.http
       .get<{ amiibo: Amiibo[] }>(url)
       .pipe(map((response) => response.amiibo));
   }
 
   searchByGameFranchise(term: string): Observable<Amiibo[]> {
-    const url = `${this.baseUrl}/amiibo/?gameseries=${term}&type=figure`;
+    const url = `${this.baseUrl}amiibo/?gameseries=${term}&type=figure`;
     return this.http
       .get<{ amiibo: Amiibo[] }>(url)
       .pipe(map((response) => response.amiibo));
   }
 
   searchAmiiboByHead(tailCode: string): Observable<Amiibo | null> {
-    const url = `${this.baseUrl}/amiibo/?tail=${tailCode}&type=figure`;
+    const url = `${this.baseUrl}amiibo/?tail=${tailCode}&type=figure`;
     return this.http.get<{ amiibo: Amiibo[] }>(url).pipe(
       map((response) => {
         const amiibos = response.amiibo;
